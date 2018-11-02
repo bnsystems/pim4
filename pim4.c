fe7c9656ec81fa3f581b6efbac4039fbaf761e8f
nelseixas <nelson.seixas@gmail.com>
2018-11-02 00:09:50 -0300

    Validação das respostas de idade e profissão
    Foi adicionado um teste de validação para a resposta da idade da pessoa e de sua profissão como professor da rede de ensino pública.

--- pim4.c
+++ pim4.c
@@ -1,10 +1,8 @@
 /*Programa ingressos_BNS.c
 
-Descrição: Este é um sistema pela BNS Desenvolvimento de Sistemas para venda de ingressos de teatro desenvolvidos na linguagem C tal como definida no padrão ISO89/90.
+Descrição: Este é um sistema pela BN Systems para venda de ingressos de teatro desenvolvidos na linguagem C tal como definida no padrão ISO89/90.
 
-A escolha de ISO C 89/90 se justifica pela necessidade de garantir plena portabilidade do sistema entre máquinas rodando sistema operacional MS-Windows (que só é compatível com ISO C89/90) e máquinas operando outros sistemas operacionais.  Assim, este código foi compilado com sucesso em uma máquina 
-LUBUNTU Linux 64b (usando a suíte de compilação gcc) e em uma máquina MS-Windows 10 64 (usando o 
-compilador mingw-w64 ).
+A escolha de ISO C 89/90 se justifica pela necessidade de garantir plena portabilidade do sistema entre máquinas rodando sistema operacional MS-Windows (que só é compatível com ISO C89/90) e máquinas operando outros sistemas operacionais.  Assim, este código foi compilado com sucesso em uma máquina LUBUNTU Linux 64b (usando a suíte de compilação gcc) e em uma máquina MS-Windows 10 64 (usando o compilador mingw-w64 ).
 
 O sistema fica no ar até ser desligado por meio da digitação do código secreto de desligamento no lugar do nome do usuário.  O código de desligamento é:
 
@@ -29,16 +27,25 @@ void main ()
 	int idadeCliente;
 	char diaSemana;
 	float desconto = 0;
-	char professorRedePublicaEnsino = "N"
+	char professorRedePublicaEnsino
+	int estadoSistema = "ligado"
 	
-	while(True){
+	while(estadoSistema == "ligado"){
 		printf("\nCompra aqui seu ingresso\n\n");
-		printf("Qual a sua idade?\n\n");
+		printf("Qual a sua idade em anos?\n\n");
 		scanf("%d\n\n",&idadeCliente);
-		printf("Você é professor da rede pública de ensino? Responda S (para SIM) ou N (para não).\n\n");
+		while(idadeCliente != (int)idadeCliente){	
+			prinf("Você não digitou uma idade válida!  Tente de novo.");
+			printf("Qual a sua idade em anos?\n\n");
+			scanf("%d\n\n",&idadeCliente);	
+		}
+		printf("Você é professor da rede pública de ensino? Para responder SIM, digite S (MAIÚSCULO); para NÃO, digite N (MAIÚSCULO).\n\n");
 		scanf("%c",&professorRedePublicaEnsino);
-		if(professorRedePublicaEnsino != "S" || professorRedePublicaEnsino != "N"){
-			printf("Use letras maiúsculas!!!");
+		while(professorRedePublicaEnsino != "S" || "N"){
+			prinf("Você não digitou uma resposta válida!  Tente de novo.");	
+			printf("Você é professor da rede pública de ensino? Para responder SIM, digite S (MAIÚSCULO); para NÃO, digite N (MAIÚSCULO).\n\n");
+			scanf("%c",&professorRedePublicaEnsino);
+		}
 		
 		
 		
